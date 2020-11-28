cd8a32f7d6bbfd3ece63744715d9f9b2e3a0d66c
dovoltaga <dovoltaga@yahoo.co.jp>
2020-11-28 22:13:42 +0900

    change keymap

--- keyboards/akbd2010/keymaps/dovoltaga/keymap.c
+++ keyboards/akbd2010/keymaps/dovoltaga/keymap.c
@@ -22,35 +22,35 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  | Bksp |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
+ * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |  :   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  UP  |Shift |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  UP  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | FKEY | ZKHK | Alt  | MHEN |Lower | Space| HENK |Raise | FKEY | Left | Down |Right |
+ * | FKEY1| ZKHK |  OS  | ALT  |Lower | Space| HENK |Raise | FKEY2| Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_ortho_4x12(
    KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_ENT, \
-   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_RSFT , \
-   FKEY,    JP_ZKHK, KC_LALT, JP_MHEN, LOWER,   KC_SPC,  JP_HENK, RAISE,   FKEY,    KC_LEFT, KC_DOWN, KC_RGHT \
+   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, JP_COLN, \
+   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   KC_ENT, \
+   FKEY,    JP_ZKHK, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  JP_HENK, RAISE,   FKEY,    KC_LEFT, KC_DOWN, KC_RGHT \
 ),
 
 /* Lower
  * ,-----------------------------------------------------------------------------------.
  * | TAB  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  |  @   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |   -  |   ^  |   \  |      |   ;  |  :   |
+ * |      |      |   `  |   |  |   ~  |   =  |   -  |   ^  |   \  |      |   ;  |  :   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |  [   |   ]  |  ,   |  .   |  /   |  \   |
+ * |      |      |   ?  |   _  |   {  |   }  |  [   |   ]  |  ,   |  .   |  /   |  \   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_ortho_4x12( \
   KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    JP_AT, \
-  _______, _______, _______, _______, _______, _______, JP_MINS, JP_CIRC, JP_YEN , _______, JP_SCLN, JP_COLN, \
-  _______, _______, _______, _______, _______, _______, JP_LBRC, JP_RBRC, KC_COMM, KC_DOT , KC_SLSH, JP_BSLS, \
+  _______, _______, JP_GRV,  JP_PIPE, JP_TILD, JP_EQL,  JP_MINS, JP_CIRC, JP_YEN , _______, JP_SCLN, JP_COLN, \
+  _______, _______, JP_QUES, JP_UNDS, JP_LCBR, JP_RCBR, JP_LBRC, JP_RBRC, KC_COMM, KC_DOT , KC_SLSH, JP_BSLS, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______  \
 ),
 
@@ -58,36 +58,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * | TAB  |  !   |  "   |  #   |  $   |  %   |  &   |   '  |  (   |  )   |      |  `   |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |  =   |   ~  |   |  |      |   +  |  *   |
+ * |      |      |  @   |  \   |  ^   |  -   |  =   |   ~  |   |  |      |   +  |  *   |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |      |      |      |      |      |  {   |   }  |   <  |   >  |   ?  |  _   |
+ * |      |      |  /   |  \   |  [   |  ]   |  {   |   }  |   <  |   >  |   ?  |  _   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_ortho_4x12( \
   KC_TAB,  JP_EXLM, JP_DQUO, JP_HASH, JP_DLR,  JP_PERC, JP_AMPR, JP_QUOT, JP_LPRN, JP_RPRN, _______, JP_GRV, \
-  _______, _______, _______, _______, _______, _______, JP_EQL,  JP_TILD, JP_PIPE, _______, JP_PLUS, JP_ASTR, \
-  _______, _______, _______, _______, _______, _______, JP_LCBR, JP_RCBR, JP_LABK, JP_RABK, JP_QUES, JP_UNDS, \
+  _______, _______, JP_AT,   JP_YEN,  JP_CIRC, JP_MINS, JP_EQL,  JP_TILD, JP_PIPE, _______, JP_PLUS, JP_ASTR, \
+  _______, _______, JP_SLSH, JP_BSLS, JP_LBRC, JP_RBRC, JP_LCBR, JP_RCBR, JP_LABK, JP_RABK, JP_QUES, JP_UNDS, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
 
 /* FKEY
  * ,-----------------------------------------------------------------------------------.
- * | TAB  |  F1  |  F2  |  F3  |  F4  | MHEN |      |      |  PSc | ScrLk| Pus  | DEL  |
+ * |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |  F7  |  F8  |  F9  |  F10 | F11  | DEL  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | CAPS |  F5  |  F6  |  F7  |  F8  | HENK |  *   |  /   | HOME | PgUp |      | INS  |
+ * | CAPS |  1   |  2   |  3   |  4   |  5   |  Pus | PSc  | HOME | PgUp | F12  |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F9  | F10  |  F11 |  F12 | Kana |  +   |  -   | END  | PgDn | Shift|      |
+ * |      |  6   |  7   |  8   |  9   |  0   |      | ScrLk| END  | PgDn | Shift| INS  |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  OS  |      |      |      |      |      |      |      | DEL  |  OS  | Ctrl |
+ * |      |  .   |      |      |      | MHEN | KANA |      |      | DEL  |  OS  | Ctrl |
  * `-----------------------------------------------------------------------------------'
  */
 [_FKEY] = LAYOUT_ortho_4x12( \
-  KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   JP_MHEN, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, KC_DEL, \
-  KC_CAPS, KC_F5,   KC_F6,   KC_F7,   KC_F8,   JP_HENK, JP_ASTR, KC_SLSH, KC_HOME, KC_PGUP, _______, KC_INS, \
-  _______, KC_F9,   KC_F10,  KC_F11,  KC_F12,  JP_KANA, JP_PLUS, KC_MINS, KC_END,  KC_PGDN, KC_RSFT, _______, \
-  _______, KC_LGUI, _______, _______, _______, _______, _______, _______, _______, KC_DEL,  KC_RGUI, KC_RCTL \
+  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_DEL, \
+  KC_CAPS, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_PAUS, KC_PSCR, KC_HOME, KC_PGUP, KC_F12,  _______, \
+  _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______, KC_SLCK, KC_END,  KC_PGDN, KC_RSFT, KC_INS, \
+  _______, KC_DOT, _______, _______, _______,  JP_MHEN, JP_KANA, _______, _______, KC_DEL,  KC_RGUI, KC_RCTL \
 ),
 
 /* Adjust (Lower + Raise)
