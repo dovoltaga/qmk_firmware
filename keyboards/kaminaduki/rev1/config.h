32d18955aaf9d365620246e8066ad0b0094049b7
dovoltaga <dovoltaga2choppy@yahoo.co.jp>
2021-04-27 20:59:15 +0900

    change VENDOR_ID

--- keyboards/kaminaduki/rev1/config.h
+++ keyboards/kaminaduki/rev1/config.h
@@ -19,7 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #pragma once
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x6F77
+// #define VENDOR_ID       0x6F77
+#define VENDOR_ID       0x644F //dO
 #define PRODUCT_ID      0x0002
 #define DEVICE_VER      0x0002
 #define MANUFACTURER    dovoltaga
